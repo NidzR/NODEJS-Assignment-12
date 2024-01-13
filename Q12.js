4422bc1beca7bfc0c81a04c2a18d2e35d7638926
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:02:19 +0500

    NODEJS Assignment Q12
    12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Q12.js
@@ -0,0 +1,7 @@
+"use strict";
+//11. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ["Rizwan Akhatr", "Ahsen Rizwan", "Nida Rizwan"];
+let message = "is the Founder of IT Academy of Nida Rizwan ";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
---
+++ Q12.ts
@@ -0,0 +1,7 @@
+//11. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[] = ["Rizwan Akhatr", "Ahsen Rizwan", "Nida Rizwan"];
+let message: string = "is the Founder of IT Academy of Nida Rizwan ";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
